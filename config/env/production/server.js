c980a7f63a7f01af0e3b4c2864c72f4005ab525d
Jaivik <jaivikjoshi7@gmail.com>
2025-03-04 14:56:08 -0500

    Fix APP_KEYS configuration for Render deployment

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env('RENDER_EXTERNAL_URL'),
   proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2', 'defaultKey3', 'defaultKey4']),
   },
 });
